fcfcfe08706a2598c30756da6c223add2f1b7086
Friso Homan <fhoman@gmail.com>
2020-06-12 18:54:30 +0200

    worked on new video mail

--- routes/admin/adminRoutes.js
+++ routes/admin/adminRoutes.js
@@ -4,26 +4,13 @@ const updateVideos = require('../../updateVideos')
 const Videos = require("../../models/video");
 const User = require("../../models/user");
 const mailNewUser = require("../../mailservices/mailNewUser")
+const mailNewVideo = require("../../mailservices/mailNewVideo")
 
-
-// Add a new user by administrator and send a e-mail with link for signup
+// Invite a new user by administrator and send a e-mail with link for signup
 
 adminRoutes.post('/mail-user', (req, res, next) => {   
-console.log(req.body.email)
-  mailNewUser(req.body.email)
-    
+  mailNewUser(req.body.email)    
   });
-
-// Add a student to the list
-
-adminRoutes.post('/add-student',(req,res,next) => {
-  console.log(req.body.user)
-
-User.create({username:req.body.user})
-
-
-  })
-
 
 // Update the list with YouTube videos in the database
 
@@ -31,7 +18,6 @@ adminRoutes.get('/updatevideos',(req,res,next) => {
 updateVideos()
 Videos.find()
 .then(response => {
-
     res.json(response);
 } )
   
@@ -55,7 +41,6 @@ Videos.find().populate('users')
 // Show all students that not already tagged for a video in the admin view
 
 adminRoutes.post('/students-video',(req,res,next) => {
-
 Videos.findById(req.body.videoID)
 .then(video => {  
   User.find({
@@ -69,18 +54,13 @@ Videos.findById(req.body.videoID)
 
 // Get a video in the admin view
 
-adminRoutes.post('/video',(req,res,next) => {
- 
+adminRoutes.post('/video',(req,res,next) => { 
   Videos.findById(req.body.videoID).populate("users")
   .then(video => res.json(video))
   .catch(err => console.log(err))
   })
 
-
-  
-
 // Tag a selected video and add it to a user video array and a video user array
-
 adminRoutes.post('/tagvideo',(req,res,next) => {
   console.log(req.body)
 const {videoID,username} = req.body
@@ -102,11 +82,8 @@ const videoUpdate = Videos.findOneAndUpdate(
 })   
 })
 
-
-
     
 // Tag a selected video and delete it from the user video array
-
 adminRoutes.post('/untagvideo',(req,res,next) => {
   console.log(req.body)
     const {videoID,username} = req.body   
@@ -131,15 +108,33 @@ Promise.all([userDelete, videoDelete]).then((values) => {
     })
   })   
 
-// Tag a selected video and delete it from the user video array
-
-adminRoutes.post('/mailstudents',(req,res,next) => {
-    
 
 
+// Send notifications to student(s) after they are tagged on a video
+
+adminRoutes.post('/send-notification',(req,res,next) => { 
+  const {taggedStudentsArr,videoID} = req.body
+// Get array with ID's students and ID video
+taggedStudentsArr.forEach(element => {
+User.find({username:element})
+.then(user => {
+  if (user[0].emailnotifications) {
+
+      mailNewVideo(user[0],videoID)
+  }
+}
+  )
+.catch(err => console.log(err))
+  
+});
+
+// Loop over array and send them a e-mail if notification is true
 
 
-    })
+// Trigger email function for every student and pass the email, the Video details and the profile page
+
+
+  })
 
 
 
